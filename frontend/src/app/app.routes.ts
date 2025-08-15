8abc4071991bd598c2fccb51735583fd1b1908ee
Gabriellealsantos <gabriel.lealsantos@hotmail.com>
2025-08-15 14:51:30 -0300

    Começando front com angular

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/components/login-component/login-component';
+import { RegistrationCustomerComponent } from './features/auth/components/registration-customer-component/registration-customer-component';
+
+export const routes: Routes = [
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegistrationCustomerComponent },
+
+
+    // { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+];
---
+++ frontend/src/app/features/auth/components/registration-customer-component/registration-customer-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-customer-component',
+  imports: [],
+  templateUrl: './registration-customer-component.html',
+  styleUrl: './registration-customer-component.css'
+})
+export class RegistrationCustomerComponent {
+
+}
