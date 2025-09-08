2857070d8590d89a7764f7147b238e8c895dd69f
Gabriel Leal Santos <gabriel.lealsantos@hotmail.com>
2025-09-07 21:52:25 -0300

    Corrigindo error de css

--- frontend/src/app/features/components/home-page-component/home-page-component.ts
+++ frontend/src/app/features/components/home-page-component/home-page-component.ts
@@ -1,34 +1,45 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Header } from "../../../shared/components/header/header";
+import { Footer } from "../../../shared/components/footer/footer";
 
+// Configuração global do Swiper
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home-page-component.html',
   styleUrls: ['./home-page-component.css'],
+  imports: [Header, Footer],
 })
 export class HomePageComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
   ngAfterViewInit() {
-    new Swiper('.swiper', {
-      modules: [Navigation, Pagination, Autoplay], // 👈 obrigatório no Swiper 11
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      slidesPerView: 1,
-      spaceBetween: 20,
-      centeredSlides: true,
-    });
+    if (isPlatformBrowser(this.platformId)) {
+      // Só executa no navegador (evita erro de "document is not defined")
+      new Swiper('.swiper', {
+        modules: [Navigation, Pagination, Autoplay],
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        centeredSlides: true,
+      });
+    }
   }
 }
